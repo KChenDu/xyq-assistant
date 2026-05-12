eb4aab909c0b459d8d1ec0250fc7e9073d8a23a3
chenkaiwen08 <chenkaiwen08@meituan.com>
2026-05-12 20:27:41 +0800

    添加宝石数量计算

--- scripts/gem_calculator.py
+++ scripts/gem_calculator.py
@@ -1,6 +1,31 @@
 from math import sqrt
 
 
+def get_gem_n(level: int) -> int:
+    assert 0 < level < 21
+    
+    if level > 19:
+        return 2 * get_gem_n(19) + get_gem_n(17) + 2 * get_gem_n(18)
+    if level > 18:
+        return 2 * get_gem_n(18) + get_gem_n(15) + get_gem_n(16) + get_gem_n(17)
+    if level > 17:
+        return 2 * get_gem_n(17) + get_gem_n(13) + get_gem_n(14) + get_gem_n(16)
+    if level > 16:
+        return 2 * get_gem_n(16) + get_gem_n(15)
+    if level > 15:
+        return 2 * get_gem_n(15) + get_gem_n(11) + get_gem_n(12) + get_gem_n(13)
+    if level > 14:
+        return 2 * get_gem_n(14) + get_gem_n(9) + get_gem_n(12)
+    if level > 13:
+        return 2 * get_gem_n(13) + get_gem_n(9) + get_gem_n(10)
+    if level > 12:
+        return 2 * get_gem_n(12) + get_gem_n(9)
+    if level > 11:
+        return 2 * get_gem_n(11) + get_gem_n(3) + get_gem_n(5) + get_gem_n(6)
+    
+    return 2 ** (level - 1)
+
+
 def get_gem_price(base_price: float, level: int) -> float:
     """获取宝石价格
 
@@ -11,28 +36,8 @@ def get_gem_price(base_price: float, level: int) -> float:
     Returns:
         float: 宝石价格
     """
-    assert base_price > 0. and 0 < level < 21
-    
-    if level > 19:
-        return 2 * get_gem_price(base_price, 19) + get_gem_price(base_price, 17) + 2 * get_gem_price(base_price, 18)
-    if level > 18:
-        return 2 * get_gem_price(base_price, 18) + get_gem_price(base_price, 15) + get_gem_price(base_price, 16) + get_gem_price(base_price, 17)
-    if level > 17:
-        return 2 * get_gem_price(base_price, 17) + get_gem_price(base_price, 13) + get_gem_price(base_price, 14) + get_gem_price(base_price, 16)
-    if level > 16:
-        return 2 * get_gem_price(base_price, 16) + get_gem_price(base_price, 15)
-    if level > 15:
-        return 2 * get_gem_price(base_price, 15) + get_gem_price(base_price, 11) + get_gem_price(base_price, 12) + get_gem_price(base_price, 13)
-    if level > 14:
-        return 2 * get_gem_price(base_price, 14) + get_gem_price(base_price, 9) + get_gem_price(base_price, 12)
-    if level > 13:
-        return 2 * get_gem_price(base_price, 13) + get_gem_price(base_price, 9) + get_gem_price(base_price, 10)
-    if level > 12:
-        return 2 * get_gem_price(base_price, 12) + get_gem_price(base_price, 9)
-    if level > 11:
-        return 2 * get_gem_price(base_price, 11) + get_gem_price(base_price, 3) + get_gem_price(base_price, 5) + get_gem_price(base_price, 6)
-    
-    return base_price * 2 ** (level - 1)
+    assert base_price > 0.
+    return base_price * get_gem_n(level)
 
 
 def get_gem_stamina(level: int) -> int:
@@ -71,6 +76,27 @@ def get_gem_stamina(level: int) -> int:
     return stamina
 
 
+def get_star_stone_n(level: int) -> int:
+    """获取星辉石数量
+
+    Args:
+        level (int): 星辉石等级
+
+    Returns:
+        int: 星辉石数量
+    """
+    assert 1 < level < 12
+    
+    if level > 10:
+        return 3 * get_star_stone_n(10) + get_star_stone_n(9)
+    if level > 9:
+        return 3 * get_star_stone_n(9) + get_star_stone_n(6) + get_star_stone_n(7)
+    if level > 8:
+        return 3 * get_star_stone_n(8) + get_star_stone_n(5)
+    
+    return 3 ** (level - 1)
+
+
 def get_star_stone_price(base_price: float, level: int) -> float:
     """获取星辉石价格
 
@@ -81,16 +107,8 @@ def get_star_stone_price(base_price: float, level: int) -> float:
     Returns:
         float: 星辉石价格
     """
-    assert base_price > 0. and 0 < level < 12
-    
-    if level > 10:
-        return 3 * get_star_stone_price(base_price, 10) + get_star_stone_price(base_price, 9)
-    if level > 9:
-        return 3 * get_star_stone_price(base_price, 9) + get_star_stone_price(base_price, 6) + get_star_stone_price(base_price, 7)
-    if level > 8:
-        return 3 * get_star_stone_price(base_price, 8) + get_star_stone_price(base_price, 5)
-    
-    return base_price * 3 ** (level - 1)
+    assert base_price > 0.
+    return base_price * get_star_stone_n(level)
 
 
 def get_star_stone_stamina(level: int) -> int:
@@ -117,6 +135,21 @@ def get_star_stone_stamina(level: int) -> int:
     return stamina
 
 
+
+def get_spirit_dust_n(level: int) -> int:
+    """获取五色灵尘数量
+
+    Args:
+        level (int): 五色灵尘等级
+
+    Returns:
+        int: 五色灵尘数量
+    """
+    assert level > 0
+    sqrt2 = sqrt(2)
+    return 1 / (2 * sqrt2) * ((1 + sqrt2) ** level - (1 - sqrt2) ** level)
+
+
 def get_spirit_dust_price(base_price: float, level: int) -> float:
     """获取五色灵尘价格
 
@@ -128,9 +161,7 @@ def get_spirit_dust_price(base_price: float, level: int) -> float:
         float: 五色灵尘价格
     """
     assert base_price > 0. and level > 0
-    
-    sqrt2 = sqrt(2)
-    return base_price / (2 * sqrt2) * ((1 + sqrt2) ** level - (1 - sqrt2) ** level)
+    return base_price * get_spirit_dust_n(level)
 
 
 def get_spirit_dust_stamina(level: int) -> int:
